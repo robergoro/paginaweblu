c807ec540427ec48464f295fed0b6e3d49e6f720
Roberto <gorostizups3@gmail.com>
2022-08-02 01:53:40 -0300

    2/08/2022

---
+++ LucianaWeb/local.py
@@ -0,0 +1,20 @@
+from .settings import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lupagina',
+        'USER' : 'root',
+        'PASSWORD': 'root',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
+    }
+}
